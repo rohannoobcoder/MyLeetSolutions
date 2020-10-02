60f13194cda255b09f51c6014463cb3abe088a9f
Rohan Singh <1andonly1rohan@gmail.com>
2020-10-02 17:56:21 +0530

    Maximum length of repeated subarray

---
+++ Array/MaximumLengthOfRepeatedSubarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findLength(vector<int>& A, vector<int>& B) {
+        vector<vector<int>>dp(A.size()+1,vector<int>(B.size()+1));
+        int ans=0;
+        for(int i=A.size()-1;i>=0;i--){
+            for(int j=B.size()-1;j>=0;j--){
+                if(A[i]==B[j]){
+                    dp[i][j]=dp[i+1][j+1]+1;
+                    if(dp[i][j]>ans){
+                        ans=dp[i][j];
+                    }  
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
